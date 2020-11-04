e8af16c46c7af6aebf97eec963dd5e9eb7120e37
Herwin Torres <hertosan24@hotmail.com>
2020-11-04 10:51:49 -0500

    cambio en mi-archivo.js
    cambio en el condicional if

--- mi-archivo.js
+++ mi-archivo.js
@@ -1,5 +1,7 @@
 alert("Hola mundo");
 var comando = "repositorio";
-if(comando === "repositorio"){
-  console.log("True");
+if (comando === "repositorio") {
+    console.log("True");
+} else {
+    console.log(false);
 }
